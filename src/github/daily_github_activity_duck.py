748ebf8fc00b9d63aea2606de0433b0f5feb5d19
wanghaisheng <edwin_uestc@163.com>
2024-12-23 20:15:38 +0800

    fix path

--- src/github/daily_github_activity_duck.py
+++ src/github/daily_github_activity_duck.py
@@ -500,7 +500,7 @@ async def main():
         # Get the username from config
         username = config.get("username", "default_username")
         print('start to detect all repos')
-        repos = get_repositories(username)
+        repos = get_repositories(username)[:10]
         if not repos:
             print("No repositories found or failed to fetch repositories.")
             return
